1ff472ecbbb0cab6e0156d31b1294eb0d03d67a7
Kağan Berke Maktav <controleng@yandex.com>
2023-09-09 14:49:17 +0300

    customer username logged

--- src/main/java/com/berkemaktav/basicstoreapp/service/CustomerService.java
+++ src/main/java/com/berkemaktav/basicstoreapp/service/CustomerService.java
@@ -2,17 +2,21 @@ package com.berkemaktav.basicstoreapp.service;
 
 import com.berkemaktav.basicstoreapp.model.Customer;
 import com.berkemaktav.basicstoreapp.repository.CustomerRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private static final Logger logger= LogManager.getLogger(ProductService.class);
 
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     public void createCustomer(Customer customer){
-    customerRepository.save(customer);
+        logger.info("Customer will save with this username: {}",customer.getUsername());
+        customerRepository.save(customer);
     }
 }
